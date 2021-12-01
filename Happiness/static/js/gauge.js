908665a35f1cadd3515cf5bd93b6a471b800c09d
isabella-helliwell <85843030+isabella-helliwell@users.noreply.github.com>
2021-11-30 20:30:31 -0500

    Add files via upload

---
+++ Happiness/static/js/gauge.js
@@ -0,0 +1,7 @@
+// import the data from data.js
+var gaugeElement = document.getElementsByTagName('')[0];
+
+gaugeElement.setAttribute('data-border-radius', 20);
+gaugeElement.setAttribute('data-color-numbers', 'red');
+gaugeElement.setAttribute('data-type', 'radial-gauge');
+gaugeElement.setAttribute('data-type', 'linear-gauge');
